834b6acbd351a4286fee11e042059ff0a10d2503
Gabriel Aumala <gaumala@protonmail.com>
2021-11-17 23:58:10 -0500

    Fix race condition with error results

--- app/src/main/java/com/gaumala/openjisho/frontend/dict/actions/PostSearchResults.kt
+++ app/src/main/java/com/gaumala/openjisho/frontend/dict/actions/PostSearchResults.kt
@@ -10,97 +10,172 @@ data class PostSearchResults(val result: DictSearchResult)
 
     override fun update(state: DictState): Update<DictState, DictSideEffect> {
         return when (result) {
-            is DictSearchResult.Entries -> {
-               val currentEntries = state.entryResults
-                if (currentEntries is EntryResults.Loading
-                    && currentEntries.queryText == result.queryText) {
-                    // We are setting the first page of dict entries
-                    val pagination =
-                        if (result.canLoadMore)
-                            PaginationStatus.canLoadMore
-                        else
-                            PaginationStatus.complete
-                    val newEntries = EntryResults.Ready(
-                        result.queryText, pagination, result.list
-                    )
-                    // Add a side effect to load suggestions
-                    val jmDictEntries = newEntries.items
-                        .filterIsInstance<EntryResult.JMdict>()
-                    val sideEffect: DictSideEffect =
-                        DictSideEffect.GetSuggestions(
-                            result.queryText, jmDictEntries
-                        )
+            is DictSearchResult.Entries ->
+               updateWithEntries(state, result)
 
-                    val newState = state.copy(entryResults = newEntries)
-                    Update(newState, sideEffect)
+            is DictSearchResult.Sentences ->
+                updateWithSentences(state, result)
 
-                } else if (currentEntries is EntryResults.Ready
-                    && currentEntries.isLoadingMoreWith(result.queryText)) {
-                    // We are appending a new page of dict entries
-                    val newState = state.copy(
-                        entryResults = currentEntries.addPage(
-                            result.list, result.canLoadMore
-                        )
-                    )
-                    Update(newState)
+            is DictSearchResult.Error ->
+                updateWithError(state, result)
+        }
+    }
 
-                } else
-                    Update(state)
-            }
-
-            is DictSearchResult.Sentences -> {
-                val currentSentences = state.sentenceResults
-                val newState = if (currentSentences is SentenceResults.Loading
-                    && currentSentences.queryText == result.queryText) {
-                    val pagination =
-                        if (result.canLoadMore)
-                            PaginationStatus.canLoadMore
-                        else
-                            PaginationStatus.complete
-                    val newSentences = SentenceResults.Ready(
-                        result.queryText, pagination, result.list
-                    )
-                    state.copy(sentenceResults = newSentences)
-                }
-
-                else if (currentSentences is SentenceResults.Ready
-                    && currentSentences.isLoadingMoreWith(result.queryText))
-                    state.copy(
-                        sentenceResults = currentSentences.addPage(
-                            result.list, result.canLoadMore
-                        )
-                    )
+    private fun updateWithSentences(
+        state: DictState,
+        result: DictSearchResult.Sentences
+    ): Update<DictState, DictSideEffect> {
+        val currentSentences = state.sentenceResults
+        val newState = if (currentSentences is SentenceResults.Loading
+            && currentSentences.queryText == result.queryText) {
+            val pagination =
+                if (result.canLoadMore)
+                    PaginationStatus.canLoadMore
                 else
-                    state
-                return Update(newState)
-            }
+                    PaginationStatus.complete
+            val newSentences = SentenceResults.Ready(
+                result.queryText, pagination, result.list
+            )
+            state.copy(sentenceResults = newSentences)
+        }
 
-            is DictSearchResult.Error -> {
-                val newState = if (result.isSentence)
-                    state.copy(
-                        sentenceResults = SentenceResults.Error(
-                            result.queryText,
-                            result.message
-                        )
+        else if (currentSentences is SentenceResults.Ready
+            && currentSentences.isLoadingMoreWith(result.queryText))
+            state.copy(
+                sentenceResults = currentSentences.addPage(
+                    result.list, result.canLoadMore
+                )
+            )
+        else
+            state
+        return Update(newState)
+    }
+
+    private fun updateWithEntries(
+        state: DictState,
+        result: DictSearchResult.Entries
+    ): Update<DictState, DictSideEffect> {
+        val currentEntries = state.entryResults
+        return if (currentEntries is EntryResults.Loading
+            && currentEntries.queryText == result.queryText) {
+            // We are setting the first page of dict entries
+            val pagination =
+                if (result.canLoadMore)
+                    PaginationStatus.canLoadMore
+                else
+                    PaginationStatus.complete
+            val newEntries = EntryResults.Ready(
+                result.queryText, pagination, result.list
+            )
+            // Add a side effect to load suggestions
+            val jmDictEntries = newEntries.items
+                .filterIsInstance<EntryResult.JMdict>()
+            val sideEffect: DictSideEffect =
+                DictSideEffect.GetSuggestions(
+                    result.queryText, jmDictEntries
+                )
+
+            val newState = state.copy(entryResults = newEntries)
+            Update(newState, sideEffect)
+
+        } else if (currentEntries is EntryResults.Ready
+            && currentEntries.isLoadingMoreWith(result.queryText)) {
+            // We are appending a new page of dict entries
+            val newState = state.copy(
+                entryResults = currentEntries.addPage(
+                    result.list, result.canLoadMore
+                )
+            )
+            Update(newState)
+
+        } else
+            Update(state)
+    }
+
+    private fun updateWithError(
+        state: DictState,
+        result: DictSearchResult.Error
+    ): Update<DictState, DictSideEffect> {
+        val newState = when {
+            result.isSentence ->
+                createSentencesErrorState(state, result)
+            else ->
+                createEntriesErrorState(state, result)
+        }
+        return Update(newState)
+    }
+
+    private fun createSentencesErrorState(
+        state: DictState,
+        error: DictSearchResult.Error
+    ): DictState {
+        return when (val sentenceResults = state.sentenceResults) {
+            is SentenceResults.Loading -> {
+                if (sentenceResults.queryText != error.queryText) state
+                else state.copy(
+                    sentenceResults = SentenceResults.Error(
+                        error.queryText,
+                        error.message
                     )
-                else {
-                    if (result.suggestedQueries.isEmpty())
-                        state.copy(
-                            entryResults = EntryResults.Error(
-                                result.queryText,
-                                result.message
-                            )
-                        )
-                    else // handle dict query suggestions as a special error
+                )
+            }
+            is SentenceResults.Ready -> {
+                if (sentenceResults.queryText != error.queryText) state
+                else state.copy(
+                    sentenceResults = SentenceResults.Error(
+                        error.queryText,
+                        error.message
+                    )
+                )
+            }
+            else -> return state
+        }
+    }
+
+    private fun createEntriesErrorState(
+        state: DictState,
+        error: DictSearchResult.Error
+    ): DictState {
+        return when (val entryResults = state.entryResults) {
+            is EntryResults.Loading -> {
+                when {
+                    entryResults.queryText != error.queryText -> state
+                    error.suggestedQueries.isNotEmpty() -> {
                         state.copy(
                             entryResults = EntryResults.ErrorWithSuggestions(
-                                result.queryText,
-                                result.suggestedQueries
+                                error.queryText,
+                                error.suggestedQueries
                             )
                         )
+                    }
+                    else -> state.copy(
+                        entryResults = EntryResults.Error(
+                            error.queryText,
+                            error.message
+                        )
+                    )
                 }
-                return Update(newState)
             }
+            is EntryResults.Ready -> {
+                when {
+                    entryResults.queryText != error.queryText -> state
+                    error.suggestedQueries.isNotEmpty() -> {
+                        state.copy(
+                            entryResults = EntryResults.ErrorWithSuggestions(
+                                error.queryText,
+                                error.suggestedQueries
+                            )
+                        )
+                    }
+                    else -> state.copy(
+                        entryResults = EntryResults.Error(
+                            error.queryText,
+                            error.message
+                        )
+                    )
+                }
+            }
+            else -> return state
         }
     }
 }
