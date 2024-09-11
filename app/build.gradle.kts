4f7b525dfaf1724e7bdbbf7c5ecb4c87afd5b571
Gabriel Aumala <gaumala@protonmail.com>
2024-09-11 00:43:02 -0500

    fix applicationId

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.gaumala.roomdemo"
+        applicationId = "com.gaumala.openjisho"
         minSdk = 24
         targetSdk = 34
         versionCode = 4
