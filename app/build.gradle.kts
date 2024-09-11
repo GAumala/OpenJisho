35a73603dfa38febbd84eac9b308430916456968
Gabriel Aumala <gaumala@protonmail.com>
2024-09-11 00:46:52 -0500

    Bump version 1.01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.gaumala.openjisho"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.0"
+        versionCode = 5
+        versionName = "1.01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
